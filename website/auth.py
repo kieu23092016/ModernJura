b5443639b5e8137ba2805ea3d2cae227a4ea2449
kieu23092016 <kieu23092016@gmail.com>
2021-05-02 22:16:48 +0700

    message for sign-up

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,36 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login2.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout<p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("invalid email", category="error")
+            pass
+        elif len(firstName) < 4:
+            flash("invalid name", category="error")
+            pass
+        elif password1 != password2:
+            flash("password dont match", category="error")
+            pass
+        else:
+            flash("account created", category="success")
+            #add user to database
+            pass
+
     return render_template("sign-up.html")
