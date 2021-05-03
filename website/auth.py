bb8bd1b823bff73941608da47dae6c83e4b32d76
duyen0207 <duyeenlee2072001@gmail.com>
2021-05-03 08:36:30 +0700

    fix login lan 1

--- website/auth.py
+++ website/auth.py
@@ -1,29 +1,41 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-# from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
-@auth.route('/login', methods = ['GET', 'POST'])
+
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    user = User.query.filter_by(email = email).first()
-    if user:
-        if user.password == password:
-            flash("loggin successful", category="success")
-            return redirect(url_for('views.homePage'))
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.homePage'))
+            else:
+                flash('Incorrect password, try again.', category='error')
         else:
-            flash("wrong password", category="error")
-    else:
-        flash("wrong email", category="error")
-    return render_template("login2.html")
+            flash('Email does not exist.', category='error')
+
+    return render_template("login2.html", user=current_user)
+
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout<p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
-@auth.route('/sign-up', methods = ['GET','POST'])
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
@@ -33,24 +45,24 @@ def sign_up():
 
         user = User.query.filter_by(email=email).first()
         if user:
-            flash("email already exist", category="error")
-            pass
+            flash('Email already exists.', category='error')
         elif len(email) < 4:
-            flash("invalid email", category="error")
-            pass
+            flash('Email must be greater than 4 characters.', category='error')
         elif len(firstName) < 4:
-            flash("invalid name", category="error")
-            pass
+            flash('First name must be greater than 4 character.', category='error')
         elif password1 != password2:
-            flash("password dont match", category="error")
-            pass
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 4:
+            flash('Password must be at least 4 characters.', category='error')
         else:
             "hash - tạo hashpassword chỉ có thể kiểm tra password đúng bằng cách chuyển pass-> hashpass?? for what "
-            new_user = User(email=email, password=password1)
+
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(
+                password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            flash("account created", category="success")
-            #add user to database
-            return redirect(url_for('auth.login'))
+            login_user(new_user, remember=True)
+            flash('account created', category='success')
+            return redirect(url_for('views.homePage'))
 
-    return render_template("sign-up.html")
+    return render_template("sign-up.html", user=current_user)
