f085f5d3485e918c25b25323a4de7fa5125ca462
kieu23092016 <kieu23092016@gmail.com>
2021-05-08 21:01:53 +0700

    search, admin add data, main game page

--- website/views.py
+++ website/views.py
@@ -5,8 +5,9 @@
     Game"""
 
 "blue print - include URL defined"
-from flask import Blueprint, render_template, request, redirect, url_for
-
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
+from .models import Game, Img
+from . import db
 views = Blueprint('views', __name__)
 "run this function whenever go to / route"
 
@@ -16,15 +17,8 @@ def homePage():
     if request.method == 'POST':
         search = request.form.get('search')
         print(search)
-        if search != None:
-            return redirect(url_for('views.search'))
+        return redirect(url_for('views.search', search_note=search))
     return render_template("index.html")
-
-
-@views.route('/search')
-def search():
-    return render_template("search.html")
-
 
 # user------------------------------------------------------------------------
 @views.route('/home', methods=['GET', 'POST'])
@@ -42,12 +36,37 @@ def settings():
     return render_template("settings.html")
 
 
-# link to our games ----------------------------------------------------------
-@views.route('/SnakePage')
-def snakePage():
-    return render_template("SnakePage.html")
+@views.route('/search', methods = ['get','post'])
+def search():
+    searchNote = request.args.get('search_note', None)
+    print("searchNote", searchNote)
+    findData = Game.query.filter(Game.gameName.contains(searchNote)).all()
+    if findData:
+        print("tìm thấy r :>")
+    # if request.method == 'get':
+    #     id = request.form('submit_button')
+    id = request.form.get('gamePath')
+    print('gamePath',id)
+    return render_template("search.html", notes = findData, searchNote = searchNote)
 
 
-@views.route('/SnakePlay')
-def snakePlay():
-    return render_template("Snake.html")
+@views.route('/admin', methods = ['get','post'])
+def addInfor():
+    # if request.method == "post":
+    gameName = request.form.get('gameName')
+    print(gameName)
+    description = request.form.get('description')
+    tag = request.form.get('tag')
+    gameImgPath = request.form.get('gameImgPath')
+    gamePath = request.form.get('gamePath')
+    game = Game(gameName = gameName, description = description, tag = tag, gameImgPath = gameImgPath, gamePath=gamePath)
+    db.session.add(game)
+    db.session.commit()
+    return render_template("admin.html")
+
+@views.route('/gamePage/<id>', methods = ['get','post'])
+def gamePage(id):
+    id = id
+    print('id', id)
+    game = Game.query.get(id)
+    return render_template("GamePage.html", game = game)
